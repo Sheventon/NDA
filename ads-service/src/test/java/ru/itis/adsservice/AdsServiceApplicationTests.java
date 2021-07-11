f510a5d23d806723397383f6f6eea0d4c637d3cb
Shevanton <tores.fernando.real@gmail.com>
2021-07-11 12:31:48 +0300

    Initial commit

---
+++ ads-service/src/test/java/ru/itis/adsservice/AdsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.adsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
