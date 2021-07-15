5bdd601aa1d3c512a5d7eee4d655d74cb2bc5b93
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2021-07-15 19:52:25 +0300

    migrate from zuul to spring cloud gateway

--- api-gateway/src/main/java/ru/itis/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/ru/itis/apigateway/ApiGatewayApplication.java
@@ -3,15 +3,25 @@ package ru.itis.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
 
+/**
+ * disabled @EnableZuulProxy - the most terrible thing in the world
+ * Zuul doesn't support WebSockets
+ * */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
+    }
+
 }
