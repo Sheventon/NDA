beb37ca77945f4cc2cfdb466664a68abb9322990
Adelya <65678470+Adelechka@users.noreply.github.com>
2021-07-17 14:33:11 +0300

    ads documentation added

--- ads-service/src/main/java/ru/itis/adsservice/controllers/AdController.java
+++ ads-service/src/main/java/ru/itis/adsservice/controllers/AdController.java
@@ -1,5 +1,7 @@
 package ru.itis.adsservice.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,16 @@ import ru.itis.adsservice.dto.AdDto;
 import ru.itis.adsservice.services.AdsService;
 
 @RestController
+@Api(value = "Ad Rest Controller",
+        description = "Controller for getting Ad by id")
 public class AdController {
 
     @Autowired
     private AdsService adsService;
 
     @GetMapping("/ads/ad/{id}")
+    @ApiOperation(value = "Get the Ad",
+            response = AdDto.class)
     public ResponseEntity<AdDto> getAd(@PathVariable Long id) {
         return ResponseEntity.ok(adsService.getById(id));
     }
